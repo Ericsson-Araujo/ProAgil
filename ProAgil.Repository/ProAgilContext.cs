7924c09b54d9a4b722c89f103ef8a2bb15ac832f
Ericsson-Araujo <ericsson_araujo@hotmail.com>
2020-12-13 23:57:20 -0300

    Iniciando - Identity

--- ProAgil.Repository/ProAgilContext.cs
+++ ProAgil.Repository/ProAgilContext.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProAgil.Domain;
+using ProAgil.Domain.Identity;
 
 namespace ProAgil.Repository
 {
-    public class ProAgilContext : DbContext
+    //<TUser, TRole, TKey, IdentityUserClaim<TKey>, IdentityUserRole<TKey>, 
+    //IdentityUserLogin<TKey>, IdentityRoleClaim<TKey>, IdentityUserToken<TKey>>
+    public class ProAgilContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>, UserRole, 
+                                                        IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public ProAgilContext(DbContextOptions<ProAgilContext> options) : base(options) {}
 
@@ -11,10 +17,26 @@ namespace ProAgil.Repository
         public DbSet<Palestrante> Palestrantes { get; set; }
         public DbSet<PalestranteEvento> PalestranteEventos { get; set; }
         public DbSet<Lote> Lotes { get; set; }
-        public DbSet<RedeSocial> RedeSocials { get; set; }
+        public DbSet<RedeSocial> RedeSociais { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+        {            
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserRole>(userRole => {
+                userRole.HasKey(ur => new {ur.UserId, ur.RoleId});  
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(u => u.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+
             modelBuilder.Entity<PalestranteEvento>()
                 .HasKey(PE => new {PE.EventoId, PE.PalestranteId});
         }
